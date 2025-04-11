6b6ca29992ee5f2cdeb65df09c274c41d61eca6f
DrHomeRender <celisherry1122@gmail.com>
2025-04-11 13:39:34 +0900

    1강 버튼 만들기

---
+++ Khala/Khala/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Khala
+//
+//  Created by Sungwoon Jung on 4/11/R7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func camera(_ sender: Any) {
+        print("카메라동작")
+    }
+    
+}
+
